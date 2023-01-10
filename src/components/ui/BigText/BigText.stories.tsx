122dc876eb1a5afc8680a76823472695619efd19
onpp <10293030+onpp@users.noreply.github.com>
2023-01-10 19:01:53 +0900

    #1: Implement BigText

---
+++ src/components/ui/BigText/BigText.stories.tsx
@@ -0,0 +1,14 @@
+import { Story, Meta } from '@storybook/react';
+import BigText, { PropType } from './BigText';
+
+export default {
+  component: BigText,
+} as Meta;
+
+const Template: Story<PropType> = (args) => <BigText {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children:
+    "(:|;|8^|&-|:'-|8-|(:-|:,-|:-|=|X|:*)(D|)|P|e|<|c|0|O|}|o|||(|((|\\)",
+};
--- src/components/ui/Card/Card.stories.tsx
+++ src/components/ui/Card/Card.stories.tsx
@@ -1,7 +1,9 @@
 import { Story, Meta } from '@storybook/react';
 import { IconPencil, IconPlay } from '../../Icon';
+import BigText from '../BigText/BigText';
 import Button from '../Button/Button';
 import CardHeader from '../CardHeader/CardHeader';
+import GachaDetail from '../GachaDetail/GachaDetail';
 import Selector from '../Selector/Selector';
 import Textarea from '../Textarea/Textarea';
 import Card, { PropType } from './Card';
@@ -37,6 +39,36 @@ Default.args = {
       正規表現にマッチする
       <br />
       ランダムな文字列が生成されます。
+    </>
+  ),
+};
+
+export const Post = Template.bind({});
+Post.args = {
+  children: (
+    <>
+      <CardHeader>
+        <span>Emoticon Generator</span>
+        <GachaDetail
+          center
+          created="2023/01/10 14:25:42"
+          name="@koluriri"
+          playCount={23}
+          src="https://pbs.twimg.com/profile_images/1558029533047300096/TGTuFAw0_400x400.jpg"
+        />
+      </CardHeader>
+      <BigText>
+        {`(:|;|8^|&-|:'-|8-|(:-|:,-|:-|=|X|:*)(D|)|P|e|<|c|0|O|}|o|||(|((|\)`}
+      </BigText>
+      <Selector
+        handleChange={(val: string) => {
+          console.log(val);
+        }}
+      />
+      <Button block variant="primary">
+        <IconPlay />
+        ガチャを引く
+      </Button>
     </>
   ),
 };
