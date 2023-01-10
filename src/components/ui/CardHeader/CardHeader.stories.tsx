ea238da41f8d30f80f5a134b7ae5fe60c6f57dec
onpp <10293030+onpp@users.noreply.github.com>
2023-01-10 17:24:17 +0900

    #1: Implement CardHeader

---
+++ src/components/ui/CardHeader/CardHeader.stories.tsx
@@ -0,0 +1,52 @@
+import { Story, Meta } from '@storybook/react';
+import { IconBulb, IconPencil } from '../../Icon';
+import GachaDetail from '../GachaDetail/GachaDetail';
+import CardHeader, { PropType } from './CardHeader';
+
+export default {
+  component: CardHeader,
+} as Meta;
+
+const Template: Story<PropType> = (args) => <CardHeader {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: (
+    <>
+      <IconPencil />
+      ガチャを作る
+    </>
+  ),
+};
+
+export const Hint = Template.bind({});
+Hint.args = {
+  children: (
+    <>
+      <IconBulb />
+      ヒント
+    </>
+  ),
+};
+
+export const NoIcon = Template.bind({});
+NoIcon.args = {
+  children: <>つくったガチャを投稿する</>,
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'lg',
+  children: (
+    <>
+      <span>ねこ語ジェネレーター</span>
+      <GachaDetail
+        center
+        created="2023/01/10 14:25:42"
+        name="@koluriri"
+        playCount={23}
+        src="https://pbs.twimg.com/profile_images/1558029533047300096/TGTuFAw0_400x400.jpg"
+      />
+    </>
+  ),
+};
