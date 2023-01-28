bafa900dda903e0719f000a444b90a6a830cc736
onpp <10293030+onpp@users.noreply.github.com>
2023-01-28 13:34:50 +0900

    fromをbyに変更

--- src/hooks/useGenerateTweet.ts
+++ src/hooks/useGenerateTweet.ts
@@ -3,7 +3,7 @@ import { Post, User } from '@prisma/client';
 const useGenerateTweet =
   () => (results: string[], post: (Post & { author: User }) | null) =>
     `${results.join('\n')}\n\n${
-      post ? `${post.title} from @${post.author.userName}` : '正規表現ガチャ'
+      post ? `${post.title} by @${post.author.userName}` : '正規表現ガチャ'
     }\n`;
 
 export default useGenerateTweet;
